b0ca2c5f93ee1514a23d6b20a2c8a1b5515f52df
Walter <109875196+guygopher@users.noreply.github.com>
2023-03-05 16:14:44 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,8 +1,17 @@
 # enter all commands in order here
 # the install.sh file will point to this file, and will write the commands in order
-# arch install is root so you dont need to add sudo to anything
+# arch install is root so no sudo stuff
 # WARNING: if you need to cd into a directory, it may cause problems.
 # contact devin for more info on this 
+
+
+
+# someone do pre install stuff
+
+
+
+
+
 pacman -S --noconfirm --needed networkmanager dhclient
 systemctl enable --now networkmanager
 echo enabled networkmanager
