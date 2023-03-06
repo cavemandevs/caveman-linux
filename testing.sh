07121e81af8eace69ff103ef62e09b921acd269b
Devin Kodituwakku <74156492+ninetyninebytes@users.noreply.github.com>
2023-03-06 01:55:41 -0700

    Testing co authoring
    just a test
    
    Co-Authored-By: Gerald <109875196+guygopher@users.noreply.github.com>

--- testing.sh
+++ testing.sh
@@ -8,3 +8,4 @@ echo "Please check the setup.sh file"
 echo "Most likely, its pointing here"
 echo "This is done as a safety fearture so that the program actually does not install itself during "testing
 echo "REMOVE ALL LINKS TO THIS FILE AND THIS FILE ITSELF WHEN DONE"
+echo testing co authoring
