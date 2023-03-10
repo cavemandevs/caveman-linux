14ce584e6d31327c3ffc9ecc08007b515847b188
gerald <caernarveron@gmail.com>
2023-03-10 10:18:18 -0700

    pillow
    test

--- rock/rock.py
+++ rock/rock.py
@@ -1,5 +1,5 @@
 import requests
-
+import os
 def search_aur(package_name):
     url = f'https://aur.archlinux.org/rpc/?v=5&type=search&arg={package_name}'
     rq = requests.get(url)
@@ -9,11 +9,11 @@ def search_aur(package_name):
         sorted_results = sorted(results, key=lambda r: r['NumVotes'], reverse=True)
         for i in range(min(3, len(sorted_results))):
             result = sorted_results[i]
-            print(f"{result['Name']}, with {result['NumVotes']} upvotes")
+            print(f"{result['Name']}, with {result['NumVotes']} upvotes. description: {result['Description']}\n")
     else:
-        print(f"Cannot find '{package_name}' on the AUR.")
+        print(f"cant find '{package_name}' on the aur.\n")
 
-package_name = input('What package are you looking for? ')
+package_name = input('what package are you looking for  \n')
 if package_name == '':
     exit()
 search_aur(package_name)
