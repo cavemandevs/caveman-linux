76b84cba0fe16c65faa729c5e3e1fd66e99c5a3b
ninetyninebytes <74156492+ninetyninebytes@users.noreply.github.com>
2023-03-27 19:22:59 -0600

    trying to fix bugs

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-pacstrap -S python
+
+# ERROR:
+# pacstrap is not installed by defualt
+# what does this even do lol
+
 chmod +x install.sh
 chmod +x testing.sh
 ./install.sh
--- testing.sh
+++ testing.sh
@@ -7,6 +7,6 @@ echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
 echo "This is the testing file"
 echo "Please check the setup.sh file"
 echo "Most likely, its pointing here"
-echo "This is done as a safety fearture so that the program actually does not install itself during "testing
+echo "This is done as a safety fearture so that the program actually does not install itself during testing"
 echo "REMOVE ALL LINKS TO THIS FILE AND THIS FILE ITSELF WHEN DONE"
 echo testing co authoring
