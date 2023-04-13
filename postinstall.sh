f7b4f3c414d5495b0d559fc1029e599063fe38e6
arch linuxer <109875196+caernarferon@users.noreply.github.com>
2023-04-12 19:13:49 -0600

    its a mess but, ill fix it later

--- postinstall.sh
+++ postinstall.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ######################################################################
-#                        * . Caveman Linux! .*                       # 
+#                        * . Caveman Linux .*                       # 
 ######################################################################
 #      A small school project built by Devin, Bradley and Soham      #
 #                         Check us out here!                         #
@@ -14,104 +14,83 @@
 #            Please read the LICENSE file for the license            #
 ######################################################################
 
+# def colors
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+NC='\033[0m' # No Color
+
+# print textlogo.txt
 echo 
 cat textlogo.txt
 echo
 echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
 echo -e "\033[1mCaveman Linux Installer\033[0m"
-echo "This will configure Arch Linux with our modifications"
+echo "This script will configure Caveman Linux."
 echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
-echo "Make sure you read the README.md before continuing."
-echo "also, please read the LICENSE file for the license."
+echo "Make sure you read the README.md before continuing. (or else)"
+echo "By continuing, you acknowledge and ."
 echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
-echo -e "\033[1mWARNING: WE ARE NOT RESPONSIBLE FOR DAMAGES TO YOUR COMPUTER.\033[0m"
-echo -e "\e[3m(so please make sure you know what you're doing!)\e[0m"
+echo -e "\033[1mWARNING: WE ARE NOT RESPONSIBLE FOR DAMAGE DONE TO YOUR COMPUTER.\033[0m"
+echo -e "\e[3m(so please make sure you know what you're doing...)\e[0m"
 echo
-echo "Please choose an option:"
-echo
-echo "S: Start the Installation"
-echo "C: Cancel the Installation"
-echo "R: View the README.md file"
 
+# Ask user for input
 while true; do
-    read -p "
-S - Start installation
-C - Cancel 
-R - Read README.md file
-
-Your Selection [S/C/R]: " scr
+    read -p "Please choose an option [S/C/R]: " scr
     case $scr in
         [Ss]* ) break;;
-        [Cc]* ) echo installation cancelled!; exit;;
+        [Cc]* ) echo "Installation cancelled!"; exit;;
         [Rr]* ) cat README.md;;
-        * ) echo "Please choose a valid selection.";;
+        * ) echo -e "${RED}Invalid input, please choose a valid option.${NC}"`;;
     esac
 done
 
+
+# Final confirmation with user
 echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
-echo -e "\033[1mCONFIRMATION\033[0m"
-echo
-echo This is a confirmation to install the software
-echo WE ARE NOT RESPONSIBLE FOR DAMAGES TO YOUR COMPUTER.
-
-while true; do
-    read -p "Do you want to Install? [Y/N]: " yn
-    case $yn in
-        [Yy]* ) break;;
-        [Nn]* ) echo "installation cancelled, please start over."; exit;;
-        * ) echo "Please choose a valid option."
-    esac
-done
-
+echo -e "\033[1mFINAL CONFIRMATION\033[0
 echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
 echo -e "\033[1mFINAL CONFIRMATION\033[0m"
 echo
 echo -e "\033[1mLAST WARNING:\033[0m"
 echo -e This the FINAL confirmation to install this software. Anything happening from now on is YOUR fault.
-echo By installing, you acknowledge that we are NOT RESPONSIBLE for any damage done to your device.
+echo By installing, you acknowledge that we are NOT RESPONSIBLE for any damage to your device.
 echo and you have read the README.md file before installing.
+echo To Install, please type in "whydididothis?" and press enter.
 
 VERIFY_PHRASE="whydididothis?"
 read -p "Please enter the phrase to continue: " user_input
 if [[ "$user_input" == "$VERIFY_PHRASE" ]]; then
-  echo "Starting Installation"
-    seconds=10
+  echo -e "\033[0;31mStarting Installation - 5 seconds to cancel \033[1;34m(Control + C)\033[0m"  seconds=10
 
-  while [ $seconds -gt 0 ]
+  seconds=5
+  while [ $seconds -gt 0 ] #countdown of death
   do
-      echo "Seconds remaining [Press Control + C to force quit]: $seconds"
+  echo -e "\033[0;31mSeconds remaining \033[1;34m[Press Control + C to quit]:\033[1;34m $seconds\033[0m"
       sleep 1
       seconds=$(( $seconds - 1 ))
   done
 
-
   echo "Installation Started!"
-
-  pacman -S --noconfirm --needed networkmanager dhclient
-  systemctl enable --now networkmanager
-  echo enabled networkmanager
 
   # we need to do some research on how to connect to peap on the network
 
-  #select best mirrors
 
-  pacman -S --noconfirm --needed pacman-contrib curl
-  pacman -S --noconfirm --needed reflector rsync grub arch-install-scripts git
-  cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.bak
 
   # installing packages
 
   pacman -Syu
-  pacman -S --noconfirm xorg xorg-server gnome neofetch firefox vim gnome-tweaks libreoffice-fresh htop git gnome-extra
+  pacman -S --noconfirm --needed xorg xorg-server gnome neofetch firefox vim gnome-tweaks libreoffice-fresh htop git gnome-extra
 
   #nobloat only installs core apps
 
   # TO BE ADDED:
   # integrate rock to system - Add rock to /bin, add rock to /opt, install yay.
 
-    #yay 
-    #cd /opt
-    #git clone - /opt https://github.com/Jguer/yay
+yay 
+cd /opt
+git clone /opt https://github.com/Jguer/yay
 
   # starting up GDM on startup
   systemctl enable gdm.service
@@ -125,4 +104,5 @@ echo Rebooting...
 sleep 0.5
 echo Rebooting...
 
+reboot
 fi
